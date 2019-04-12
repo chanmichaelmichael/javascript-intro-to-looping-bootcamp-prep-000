5f62b5849548a1c6fd73394c5f8f437721c26c43
Michael Michael <michael.ta.chan@gmail.com>
2019-04-12 04:10:24 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array){
-  for(let i=0; i<25; i++){
-    if(i===1){
+  for(let j=0; j<25; i++){
+    if(j===1){
       array.push("I am 1 strange loop")
     }
     else{
-      array.push(`I am ${i} strange loops.`)
+      array.push(`I am ${j} strange loops.`)
     }
   }
 }
